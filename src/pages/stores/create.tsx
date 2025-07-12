4beeb13eb48cb27326dc9beaf8a7bc44c17488c1
Z-bahaa <zizobahaapersonal@gmail.com>
2025-07-12 21:34:55 +0300

    stripped crerate store page

--- src/pages/stores/create.tsx
+++ src/pages/stores/create.tsx
@@ -16,51 +16,11 @@ export const StoreCreate = () => {
     >
       <Form {...formProps} layout="vertical">
         <Form.Item
-          label="Name"
-          name="name"
-          rules={[{ required: true, message: "Please enter store name" }]}
+          label="Title"
+          name="title"
+          rules={[{ required: true, message: "Please enter store title" }]}
         >
-          <Input placeholder="Enter store name" />
-        </Form.Item>
-
-        <Form.Item
-          label="Address"
-          name="address"
-          rules={[{ required: true, message: "Please enter store address" }]}
-        >
-          <Input.TextArea 
-            placeholder="Enter store address" 
-            rows={3}
-          />
-        </Form.Item>
-
-        <Form.Item
-          label="Phone"
-          name="phone"
-          rules={[
-            { required: true, message: "Please enter phone number" },
-            { pattern: /^[\+]?[1-9][\d]{0,15}$/, message: "Please enter a valid phone number" }
-          ]}
-        >
-          <Input placeholder="Enter phone number" />
-        </Form.Item>
-
-        <Form.Item
-          label="Email"
-          name="email"
-          rules={[
-            { required: true, message: "Please enter email address" },
-            { type: "email", message: "Please enter a valid email address" }
-          ]}
-        >
-          <Input placeholder="Enter email address" />
-        </Form.Item>
-
-        <Form.Item
-          label="Website"
-          name="website"
-        >
-          <Input placeholder="Enter website URL (optional)" />
+          <Input placeholder="Enter store title" />
         </Form.Item>
 
         <Form.Item
